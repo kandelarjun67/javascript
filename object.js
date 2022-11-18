529d5beb1331d5da8b7b7c0883348086346daf8e
kandelarjun67 <kandelarjun67@gmail.com>
2022-11-19 00:12:36 +0545

    object basic ideas

---
+++ object.js
@@ -0,0 +1,26 @@
+// creation of object 
+let obj = {
+    name: 'arjun',
+    age: 23,
+    hobbies: ['coding', 'swimming'],
+};
+
+console.log(obj.name);   // arjun
+
+console.log(obj.age)   // 23
+
+console.log(obj.hobbies[0])   // coding
+
+obj.surname = 'kandel'
+
+console.log(obj.surname)  // kandel
+
+console.log(obj)
+
+/*
+{
+  name: 'arjun',
+  age: 23,
+  hobbies: [ 'coding', 'swimming' ],
+  surname: 'kandel'
+}*/
